2c5299a1e4ab8c6dd939f86d321ee005554e0144
i.novikov <i.novikov@cardsmobile.ru>
2022-06-11 22:21:56 +0300

    initial commit

---
+++ sbermarket_api/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""Not official python client for product market sbermarket API."""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
