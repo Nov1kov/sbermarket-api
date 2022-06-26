06201d224cf33845d47b7a2c2c99b239ecc55e83
i.novikov <i.novikov@cardsmobile.ru>
2022-06-26 17:17:06 +0300

    add search chipest product example

---
+++ examples/search_chipest_product.py
@@ -0,0 +1,18 @@
+"""Example of code."""
+
+from sbermarket_api.client import Client
+
+if __name__ == "__main__":
+    client = Client()
+    # Получить все магазины доступные для данной точки.
+    stores = client.stores(lat=60.003526, lon=30.253471)
+    # Собрать список, сколько уникальных товаров найдется по пойску.
+    lowest_prices = {}
+    for store in stores:
+        products = store.search_products("Сок J7 томат 970 мл")
+        for product in products:
+            if not lowest_prices or all(product.price <= p for p in lowest_prices.keys()):
+                if lowest_prices and any(product.price < p for p in lowest_prices.keys()):
+                    lowest_prices.clear()
+                lowest_prices[product.price] = {"store": str(store), "url": product.canonical_url}
+    print(lowest_prices)
